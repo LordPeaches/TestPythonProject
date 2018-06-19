296ea2e4f29dca98561ee9c0d025204d1262f4cf
LordPeaches <AlexHughesGMU@gmail.com>
2018-06-19 11:20:38 -0400

    Added Comments
    Added comments to list example

--- TestCode1.py
+++ TestCode1.py
@@ -11,15 +11,18 @@ print('Whats 9 + 10?')
 print('Fuck you reply to me')
 
 # Takes a start and end values, and computes the sum off all multiples of m
-def sum_mult_in_range(start, end, m):
-    list_range = range(start, end + 1)
+def sum_mult_in_range(start, end, m): #This is the method declaration
+
+    list_range = range(start, end + 1) #This creates a list from Start to (end + 1), called 'list_range'
     sum_list = 0
-    for x in list_range:
-        if x % m == 0:
-            sum_list +=  x
-    return sum_list
+    
+    for x in list_range: #Starts iterating through our list, storing each value as 'x'
+        if x % m == 0: #If statement using Modulo
+            sum_list +=  x #Notice how sum_list must be declared outside the loop? Important concept
+    
+    return sum_list #Returns our list
 
 start = 5
 end = 25
 m = 5
-print(sum_mult_in_range(start, end, m))
+print(sum_mult_in_range(start, end, m)) #Call the method by its name, imports some variables, and prints out the return value
